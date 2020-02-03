dd99a7cd703b3972f508e9bba9449d09bc58ea71
JakeRoyRandall <jake@jakerandall.me>
2020-02-03 08:44:40 -0800

    Working Through Devise

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,13 +1,32 @@
 import React from "react"
+import { Navbar, Nav } from "react-bootstrap"
+import { BrowserRouter as Router, Switch, Link } from "react-router-dom"
+import { Home } from './Home'
 
 class App extends React.Component {
   render () {
+
     return (
-      <div>
-        Hello World
-      </div>
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+
+        </Navbar>
+        <Switch>
+        </Switch>
+      </Router>
     );
   }
 }
 
 export default App
+
+// {logged_in && <Nav.Link href={sign_out_route}>Sign Out</Nav.Link>}
+// {!logged_in && <Nav.Link href={sign_in_route}>Sign In</Nav.Link>}
+// const {
+//   signed_in,
+//   sign_in_route,
+//   sign_out_route
+// } = this.props
