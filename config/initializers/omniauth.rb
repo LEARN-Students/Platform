42440b7c59cfdd088f5ba0d18387063a65dfa1f4
mkayler777 <mkayler242@gmail.com>
2020-02-04 13:25:45 -0800

    Bulk of OmniAuth with Github

--- config/initializers/omniauth.rb
+++
@@ -1,4 +0,0 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :developer unless Rails.env.production?
-#   provider :github, Rails.application.credentials.dig(:omniauth, :github_key), Rails.application.credentials.dig(:omniauth, :github_secret)
-# end
