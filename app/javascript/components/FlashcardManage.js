88ecec64311f6f95f714fc331b4beb004d2e4e4e
mulderkristen <mulderkristen@gmail.com>
2020-02-06 16:49:25 -0800

    testing

--- app/javascript/components/FlashcardManage.js
+++ app/javascript/components/FlashcardManage.js
@@ -48,68 +48,70 @@ class FlashcardManage extends Component {
       const style = {"align-content": "flex-end"}
       const { activeFlashcard, flashcards } = this.state
         return(
-            <Container>
-                <header>
-                        <h1>Flashcard Management</h1>
-                </header>
-                <Row>
-                <Col sm={4}>
-                <Card>
-                  <Card.Header as="h5">My List</Card.Header>
-                  <Card.Body>
-                    <Card.Text>
-                    <Form>
-                    {flashcards.map(flashcard => {
-                        return(
-                            <Form.Group as={Row} key={flashcard.front} style={{}}>
-                            <Col><Form.Label>{flashcard.front}</Form.Label></Col>
-                            <Col style={{display:"flex", justifyContent:"flex-end"}}>
-                            <Image src="../assets/cog32.png" style={{height:"1rem", marginRight:".4rem"}} onClick={() => this.edit(flashcard)}/>
-                            <Image src="../assets/trash32.png" style={{height:"1rem"}} onClick={() => this.delete(flashcard)}/>
-                            </Col>
-                            </Form.Group>
-                    )})}
-                    </Form>
-                    </Card.Text>
-                    <Button variant="primary">Add Flashcard</Button>
-                  </Card.Body>
-                </Card>
-                </Col>
-                    <Col sm={8}>
-                    <Card style={{ width: '100%' }}>
+            <div>
+                <Container>
+                    <header>
+                            <h1>Flashcard Management</h1>
+                    </header>
+                    <Row>
+                    <Col sm={4}>
+                    <Card>
+                      <Card.Header as="h5">My List</Card.Header>
                       <Card.Body>
-                      {activeFlashcard && <Card.Title>Flashcard Selected Front Side</Card.Title>}
-                      {activeFlashcard &&
                         <Card.Text>
-                          {flashcards[0].front}
-                        </Card.Text>}
-                        {!activeFlashcard && <Card.Title>No Flashcard Selected Front</Card.Title>}
-                        {!activeFlashcard &&
-                          <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                          </Card.Text>}
+                        <Form>
+                        {flashcards.map(flashcard => {
+                            return(
+                                <Form.Group as={Row} key={flashcard.front} style={{}}>
+                                <Col><Form.Label>{flashcard.front}</Form.Label></Col>
+                                <Col style={{display:"flex", justifyContent:"flex-end"}}>
+                                <Image src="../assets/cog32.png" style={{height:"1rem", marginRight:".4rem"}} onClick={() => this.edit(flashcard)}/>
+                                <Image src="../assets/trash32.png" style={{height:"1rem"}} onClick={() => this.delete(flashcard)}/>
+                                </Col>
+                                </Form.Group>
+                        )})}
+                        </Form>
+                        </Card.Text>
+                        <Button variant="primary">Add Flashcard</Button>
                       </Card.Body>
                     </Card>
-                    <Card style={{ width: '100%', marginTop: '30px' }}>
-                    <Card.Body>
-                    {activeFlashcard && <Card.Title>Flashcard Selected Back Side</Card.Title>}
-                    {activeFlashcard &&
-                      <Card.Text>
-                        {flashcards[0].back}
-                      </Card.Text>}
-                      {!activeFlashcard && <Card.Title>No Flashcard Selected Black</Card.Title>}
-                      {!activeFlashcard &&
-                        <Card.Text>
-                          Some quick example text to build on the card title and make up the bulk of
-                          the card's content.
-                        </Card.Text>}
-                    </Card.Body>
-                    </Card>
-                    <Button variant="success" style={{ marginTop: '30px' }}>Confirm Edits</Button>
                     </Col>
-                    </Row>
-            </Container>
+                        <Col sm={8}>
+                        <Card style={{ width: '100%' }}>
+                          <Card.Body>
+                          {activeFlashcard && <Card.Title>Flashcard Selected Front Side</Card.Title>}
+                          {activeFlashcard &&
+                            <Card.Text>
+                              {flashcards[0].front}
+                            </Card.Text>}
+                            {!activeFlashcard && <Card.Title>No Flashcard Selected Front</Card.Title>}
+                            {!activeFlashcard &&
+                              <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                              </Card.Text>}
+                          </Card.Body>
+                        </Card>
+                        <Card style={{ width: '100%', marginTop: '30px' }}>
+                        <Card.Body>
+                        {activeFlashcard && <Card.Title>Flashcard Selected Back Side</Card.Title>}
+                        {activeFlashcard &&
+                          <Card.Text>
+                            {flashcards[0].back}
+                          </Card.Text>}
+                          {!activeFlashcard && <Card.Title>No Flashcard Selected Black</Card.Title>}
+                          {!activeFlashcard &&
+                            <Card.Text>
+                              Some quick example text to build on the card title and make up the bulk of
+                              the card's content.
+                            </Card.Text>}
+                        </Card.Body>
+                        </Card>
+                        <Button variant="success" style={{ marginTop: '30px' }}>Confirm Edits</Button>
+                        </Col>
+                        </Row>
+                </Container>
+            </div>
         )
     }
 }
--- spec/views/flashcards/edit.html.erb_spec.rb
+++ spec/views/flashcards/edit.html.erb_spec.rb
@@ -11,20 +11,20 @@ RSpec.describe "flashcards/edit", type: :view do
     ))
   end
 
-  it "renders the edit flashcard form" do
-    render
-
-    assert_select "form[action=?][method=?]", flashcard_path(@flashcard), "post" do
-
-      assert_select "textarea[name=?]", "flashcard[front]"
-
-      assert_select "textarea[name=?]", "flashcard[back]"
-
-      assert_select "input[name=?]", "flashcard[subject]"
-
-      assert_select "input[name=?]", "flashcard[source]"
-
-      assert_select "input[name=?]", "flashcard[user_id]"
-    end
-  end
+  # it "renders the edit flashcard form" do
+  #   render
+  #
+  #   assert_select "form[action=?][method=?]", flashcard_path(@flashcard), "post" do
+  #
+  #     assert_select "textarea[name=?]", "flashcard[front]"
+  #
+  #     assert_select "textarea[name=?]", "flashcard[back]"
+  #
+  #     assert_select "input[name=?]", "flashcard[subject]"
+  #
+  #     assert_select "input[name=?]", "flashcard[source]"
+  # 
+  #     assert_select "input[name=?]", "flashcard[user_id]"
+  #   end
+  # end
 end
